8addacda1e822f8ad87dc0d1c286a4d9d3b910e2
njerigathigi <njeriiterry@gmail.com>
2021-02-23 05:21:30 +0300

    updating the website with bootstrap

--- html/exercises/oop_exercise.py
+++ html/exercises/oop_exercise.py
@@ -19,31 +19,30 @@
 # Instantiate your class, make several deposits and withdrawals, and test to make sure the account can't be overdrawn.
 
 
-
-
 class Account:
+    """
+    Account object
+
+    @param owner(str)
+    
+    @param balance(int)
+    """
     def __init__(self, owner, balance):
         self.owner = owner
         self.balance = balance
     
-    def deposit(self,amount):
-        
-        print("deposit successful!Your current balance is {0}".format(amount + self.balance))
-        self.balance = amount + self.balance
-        return self.balance + amount
+    def deposit(self, amount):
+        self.balance += amount
+        print("Deposited: {0} successfully!Your current balance is {1}".format(amount, self.balance))
+        return self.balance
 
     def withdraw(self, amount):
         if amount > self.balance:
             print("Hello {1}.You do not have enough funds in your account to make this withdrawal.Your current balance is {0}".format(self.balance, self.owner))
         else:
-            print("withdrawal successful!Your current balance is {0}".format(self.balance - amount))
-            self.balance = self.balance - amount
-            return self.balance - amount
-
-    
-
-
-
+            self.balance -= amount
+            print("Withdrawn {0} successfully!Your current balance is {1}".format(amount, self.balance))
+            return self.balance
 
 
 # 1. Instantiate the class
@@ -54,34 +53,23 @@ acct1 = Account('Jose',100)
 print(acct1)
 
 
-
-
 # 3. Show the account owner attribute
 print(acct1.owner)
-
-
 
 
 # 4. Show the account balance attribute
 print(acct1.balance)
 
 
-
-
 # 5. Make a series of deposits and withdrawals
 acct1.deposit(50)
-
-
 
 
 acct1.withdraw(75)
 
 
-
-
 # 6. Make a withdrawal that exceeds the available balance
 acct1.withdraw(500)
-
 
 
 # ## Good job!
