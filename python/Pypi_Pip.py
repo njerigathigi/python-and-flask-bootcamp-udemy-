988edb553815b9b38c58118c275b6d272bbbcf7a
njerigathigi <njeriiterry@gmail.com>
2021-02-26 02:58:51 +0300

    pypi and pip

---
+++ python/Pypi_Pip.py
@@ -0,0 +1,5 @@
+#Pypi is a repository for open-source third-party Python packages.
+#use pip install at the command line to install these packages.
+#pip is a simple way to download packages at your command line directly from the pypi repository.
+# syntax: pip install package-name.
+#google search - python package for intended use.
