f37dd01ac598a3a0f157bb9f52be7a338cbf1ede
njerigathigi <njeriiterry@gmail.com>
2021-02-25 06:26:00 +0300

    creating a decorator

---
+++ python/decorator.py
@@ -0,0 +1,16 @@
+def squares(func):
+
+    def power_two():
+        return func() ** 2
+        # print("power two")
+        # return func() 
+
+    
+    return power_two()
+
+@squares
+def addition():
+
+    return 2 + 2
+
+print(addition)
