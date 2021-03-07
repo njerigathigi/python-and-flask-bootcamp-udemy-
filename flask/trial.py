855747cfc1836aab7e1ffc15934cc278a7cff86d
njerigathigi <njeriiterry@gmail.com>
2021-03-07 06:32:19 +0300

    debug=True

--- flask/trial.py
+++ flask/trial.py
@@ -12,8 +12,8 @@ def whoarewe(): #127.0.0.1:5000/whoarewe
 
 @app.route("/cuteyprofile/<name>") #variable name passed into the route decorator will be synced to the one 
 def cutey(name):                   #passed into the function ie they are the same.
-    return "<h3>my name is {0} and i am cute.</h3>".format(name.upper())
+    return "<h3>my name is {0} and i am cute.</h3>".format(name[200])
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
