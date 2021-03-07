f170af7c66855f1af2394e54102c0bc3cfb8fa29
njerigathigi <njeriiterry@gmail.com>
2021-03-07 05:48:34 +0300

    cleaning up code

--- flask/trial.py
+++ flask/trial.py
@@ -3,12 +3,10 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/") #127.0.0.1:5000
-
 def cutecat():
     return "<h1>CuteCat</h1>"
 
 @app.route("/whoarewe")
-
 def whoarewe(): #127.0.0.1:5000/whoarewe
     return "<h1>We are aureolophiles</h1>"
 
