144b463aa94a79b4c7a34e9222ad3bc8b26db6c7
njerigathigi <njeriiterry@gmail.com>
2021-03-07 05:58:59 +0300

    dynamic routing

--- flask/trial.py
+++ flask/trial.py
@@ -10,6 +10,10 @@ def cutecat():
 def whoarewe(): #127.0.0.1:5000/whoarewe
     return "<h1>We are aureolophiles</h1>"
 
+@app.route("/cuteyprofile/<name>") #variable name passed into the route decorator will be synced to the one 
+def cutey(name):                   #passed into the function ie they are the same.
+    return "<p>my name is {0}</p>".format(name)
+
 
 if __name__ == "__main__":
     app.run()
