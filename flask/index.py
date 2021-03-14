dfaeb5ae29e8269ab24781ae636ca6dc554176b4
njerigathigi <njeriiterry@gmail.com>
2021-03-15 02:00:23 +0300

    jinja templating

--- flask/index.py
+++ flask/index.py
@@ -8,7 +8,15 @@ def karibu():
 
 @app.route("/meow")
 def about():
-    return render_template("index.html") #html file has to be in a templates folder at the same level as your .py file.
+    cat1= "Nala"
+    cat2="Sarabi"
+    return render_template("index.html", pussycat=cat1, pussycat2=cat2) #html file has to be in a templates folder at the same level as your .py file.
+
+@app.route("/mylikes/<name>")
+def mylikes(name):
+    jina = name
+    return render_template("index.html", jina=jina)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
